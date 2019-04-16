29e5f966f2082f7d9f75a869a26bdf2961514825
mazharrasyad <muhazharrasyad@gmail.com>
2019-04-16 09:39:55 +0700

    Pertemuan 7

--- Pertemuan 7/cekhost3.py
+++ Pertemuan 7/cekhost3.py
@@ -1,19 +1,4 @@
 import os
-import threading
-import time
-
-class myThread (threading.Thread):
-    def __init__(self, threadName, counter):
-        threading.Thread.__init__(self)
-        self.name = threadName
-        self.counter = counter
-
-    def run(self):
-        while self.counter >= 1:
-            print("Starting %s - counter : %d" %(self.name, self.counter))
-            self.counter -= 1
-            time.sleep(1)
-        print("Exiting %s"%(self.name))
 
 fname = "ip.txt"
 fd = open(fname)
--- Pertemuan 7/thread.py
+++ Pertemuan 7/thread.py
@@ -1,24 +1,40 @@
 import threading
 import time
+import os
 
 class myThread (threading.Thread):
-    def __init__(self,threadName,counter):
+    def __init__(self,ip):
         threading.Thread.__init__(self)
-        self.name = threadName
-        self.counter = counter
+        self.ip = ip
 
-    def run(self):
-        while self.counter >= 1:
-            print("Starting %s - counter : %d "%(self.name, self.counter))
-            self.counter -= 1
-            time.sleep(1)
-        print("Exiting %s"%(self.name))
+    def run(self):        
+        status = "Down"
+        outx = None
+        ipx = ip.strip()
 
-# Create new threads
-thread1 = myThread("Thread-1", 5)
-thread2 = myThread("Thread-2", 4)
+        cmd = "ping -q -c3 %s" %(ipx)
+        fp = os.popen(cmd)
+        outx = fp.read()
 
-# Start new Threads
-thread1.start()
-thread2.start()
-print("Exiting Main Thread")
+        if outx.find(pattern) > -1 :
+            status = "Up"
+
+        cmd1 = "date +'%F %X'"    
+        fp1 = os.popen(cmd1)
+        outx1 = fp1.read()
+        outx1 = outx1.strip()
+        
+        print("Host %s is %s" %(ipx, status))
+        fd1.write("%s;%s;%s\n" %(outx1, ipx, status))    
+
+fname = "ip.txt"
+fd = open(fname)
+
+fname1 = "status.csv"
+fd1 = open(fname1, "a")
+
+pattern = "3 received"
+
+for ip in fd.readlines() :    
+    thread = myThread(ip)
+    thread.start()
