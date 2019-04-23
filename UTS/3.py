54e98811dfa6aad347367151f872aff422c06084
mazharrasyad <muhazharrasyad@gmail.com>
2019-04-23 14:26:46 +0700

    UTS No 3

--- UTS/3.py
+++ UTS/3.py
@@ -4,7 +4,7 @@ ip = input("Masukan Alamat IP atau Nama Host : ")
 status = "Down"
 cmd = os.popen("ping -qc3 %s" %(ip))
 cek = cmd.read()
-print(cek)
+
 if cek.find("3 received") > -1 :
     status = "Up"
 
